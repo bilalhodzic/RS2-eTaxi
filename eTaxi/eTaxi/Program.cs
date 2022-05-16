ccd578bb412a39c1740db3e1701cadd67b4f1e58
Bilal Hodzic <hodzic.bilal@hotmail.com>
2022-05-17 00:35:41 +0200

    config changes

--- eTaxi/eTaxi/Program.cs
+++ eTaxi/eTaxi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System.IO;
 
 namespace SpiderJob
 {
@@ -14,6 +15,10 @@ namespace SpiderJob
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseKestrel();
+                    webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
+                    webBuilder.UseUrls("http://localhost:44310", "http://192.168.0.15:44310");
+                    webBuilder.UseIISIntegration();
                     webBuilder.UseStartup<Startup>();
                 });
     }
